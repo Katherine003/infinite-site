a5e688be9b625164c8c5dde3c0fb342e5c76e505
Kat <katherine.howe@sckans.edu>
2022-10-27 12:08:33 -0500

    help

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import FAQ from './components/FAQ';
 
 function App() {
   return (
     <div className="App">
      Home 
-     <Link to="/infinite-site/account">Account</Link>
-
+     
      <BrowserRouter>
+
+     <div className="nav">
+     <Link to="/infinite-site">Home</Link>
+     <Link to="/infinite-site/account">Account</Link>
+     <Link to="/infinite-site/faq">FAQ</Link>
+     </div>
+
       <Routes>
         <Route path="/infinite-site/account" element={<div> Account </div>} />
+        <Route path="/infinite-site/faq" element={<FAQ />} />
       </Routes>
      </BrowserRouter>
     </div>
